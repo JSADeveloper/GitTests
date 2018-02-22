51c91e1e955cfaddbca3cca6a74d6d7fbe20318e
JSADeveloper <josh.stigall@justsolutionsag.ca>
2018-02-22 15:01:31 -0600

    First branch from Intellij

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,5 @@ public class Main {
     }
     //Just a comment to see if this ends in the repo. I'm adding this after it's already in the repo.
     //Another change. I just added a new folder called test in a CodingProjects Folder
+    //New Branch
 }
