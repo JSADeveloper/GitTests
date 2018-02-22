eac6bd1911e90eae817c91abb1fb7b90157722f5
JSADeveloper <josh.stigall@justsolutionsag.ca>
2018-02-22 13:21:43 -0600

    First commit from IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    //Just a comment to
+    //Just a comment to see if this ends in the repo. I'm adding this after it's already in the repo.
 }
