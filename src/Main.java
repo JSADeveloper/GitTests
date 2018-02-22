80f8371b5feb30021237889a1f0d48c9e94e7abb
JSADeveloper <josh.stigall@justsolutionsag.ca>
2018-02-22 13:26:04 -0600

    First commit from IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,5 @@ public class Main {
         System.out.println("Hello World!");
     }
     //Just a comment to see if this ends in the repo. I'm adding this after it's already in the repo.
+    //Another change. I just added a new folder called test in a CodingProjects Folder
 }
